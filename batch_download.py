672b3724eb6adb00cd0c2ea7fb502a9be8693e28
Kevin Chan <kevinkid135@users.noreply.github.com>
2017-06-23 14:33:33 -0400

    removed testing lines of code
    also added more comments to explain what each section does to non-coders

--- batch_download.py
+++ batch_download.py
@@ -2,16 +2,20 @@ import time #used to pause script
 import os #library used to open magnet link
 from selenium import webdriver #use selenium
 
-#global variables
-driverLocation = "C:/Users/Kevin/Downloads/Browsers/chromedriver.exe"
-url = "http://horriblesubs.info/shows/shigatsu-wa-kimi-no-uso/"
-quality = "1080p"
-download_format = "Magnet"
+#global variables (CHANGE THESE)
+driverLocation = "" #Location of your chrome driver
+url = "http://horriblesubs.info/shows/shigatsu-wa-kimi-no-uso/" # link to your desired anime
+quality = "1080p" # the quality that you want to download
+download_format = "Magnet" # format you want to download
+# end global variables
 
+# open the chrome driver
 browser = webdriver.Chrome(driverLocation)
 browser.get(url)
 #time.sleep(1) # Let the user actually see something!
 
+# find the episode elements and click the quality, then download format.
+# This will loop through all episodes
 links = browser.find_elements_by_link_text(quality)
 length = len(links)
 print (str(length) + " episodes found")
@@ -20,10 +24,9 @@ for link in links:
     print("Clicking '{}': {}/{}".format(quality, counter, length))
     link.click()
     counter+=1
-    time.sleep(0.1) #delay of animation
-    #break # temp
+    time.sleep(0.1) # delay of animation. Can be changed if animation takes longer or different systems
 
-print
+print # newline
 
 links = browser.find_elements_by_link_text(download_format)
 counter = 1
@@ -31,8 +34,7 @@ for link in links:
     print("Clicking '{}': {}/{}".format(download_format, counter, length))
     os.startfile(link.get_attribute("href"))
     counter+=1
-    time.sleep(0.1) #not too fast...
-    #break # temporary, only do first iteration
+    time.sleep(0.1) # allows some time for the next action to be done.
 
 #time.sleep(5) # Let the user actually see something!
 browser.quit()
